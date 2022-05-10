53f8dd7403eb52ac4b5903ad91f97a81765b3b9a
jwm0107 <feller0107@gmail.com>
2022-05-10 13:46:22 +0900

    omni

--- src/components/GameDetail.jsx
+++ src/components/GameDetail.jsx
@@ -46,7 +46,7 @@ export default function MainFlex() {
   }, []);
   /* ------------ connect wallet --------------*/
   useEffect(() => {
-    const address = `https://bnbtreasure.xyz?ref=${account}`;
+    const address = `https://bnbtreasure.netlify.app/?ref=${account}`;
     setReferalAddress(address);
     console.log("account changed", address);
   }, [account]);
